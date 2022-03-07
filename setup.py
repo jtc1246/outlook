351382bae4916446cda9d534307ad341bcbf40fc
jtc1246 <43341500+jtc1246@users.noreply.github.com>
2022-03-07 11:09:47 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='outlook',
+    version='0.9.0',
+    author='Tiancheng Jiao',
+    author_email='jtc1246@outlook.com',
+    url='https://github.com/jtc1246/outlook',
+    description='A simple email sender for Outlook',
+    packages=['outlook'],
+    install_requires=['myHttp','mySecrets','msal'],
+    python_requires='>=3',
+    platforms=["all"],
+    license='GPL-2.0 License',
+    entry_points={
+        'console_scripts': [
+            'setAccount=outlook:setAccount',
+            'sendEmail=outlook:sendEmail'
+        ]
+    }
+)
