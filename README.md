6f0786a49dffffbc407ce8df7cf9fddfdbe1394c
Tiancheng Jiao <43341500+jtc1246@users.noreply.github.com>
2022-04-27 18:48:28 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Outlook v1.0.4
+# Outlook v1.0.5
 
 ## 1. Introduction
 
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='outlook',
-    version='1.0.3',
+    version='1.0.5',
     author='Tiancheng Jiao',
     author_email='jtc1246@outlook.com',
     url='https://github.com/jtc1246/outlook',
     description='A simple email sender for Outlook',
     packages=['outlook'],
-    install_requires=['myHttp','mySecrets','msal'],
+    install_requires=['myHttp','mySecrets','msal','myBasics'],
     python_requires='>=3',
     platforms=["all"],
     license='GPL-2.0 License'
